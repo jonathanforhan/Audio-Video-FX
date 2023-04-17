93c356b708b5d5573b7405be38f74c4bf1b49a68
jonathanforhan <jonathan.forhan@gmail.com>
2023-04-17 18:05:30 -0400

    PyOpenGL Textures work

--- shaders/basic.frag
+++ shaders/basic.frag
@@ -7,7 +7,7 @@ in vec2 v_tex_coord;
 uniform sampler2D u_texture;
 
 void main() {
-    // f_color = texture(u_texture, v_tex_coord);
-    f_color = vec4(1.0, 0.0, 1.0, 1.0);
+    f_color = texture(u_texture, v_tex_coord);
+    // f_color = vec4(1.0, 0.0, 1.0, 1.0);
 };
 
--- src/gl_widget.py
+++ src/gl_widget.py
@@ -1,7 +1,15 @@
-import ctypes
-import numpy as np
-from OpenGL import GL as gl
+"""
+DOCS:
+https://pyopengl.sourceforge.net/documentation/pydoc/OpenGL.GL.html
+"""
+
 from PyQt6.QtOpenGLWidgets import QOpenGLWidget
+from ctypes import c_void_p
+import numpy as np
+
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+from PIL import Image
 
 
 class GLWidget(QOpenGLWidget):
@@ -10,73 +18,98 @@ class GLWidget(QOpenGLWidget):
         self.setMinimumSize(640, 480)
 
     def __del__(self):
-        gl.glDeleteVertexArrays(1, np.array(self.vao))
-        gl.glDeleteBuffers(1, np.array(self.vbo))
-        gl.glDeleteProgram(self.program)
+        glDeleteVertexArrays(1, np.array(self.vao))
+        glDeleteBuffers(1, np.array(self.vbo))
+        glDeleteProgram(self.program)
 
-    def initializeGL(self):
-        v_str, f_str = "", ""
-        with open("./shaders/basic.vert") as f:
+    @staticmethod
+    def __init_shaders(v_path, f_path):
+        with open(v_path) as f:
             v_str = f.read()
-        with open("./shaders/basic.frag") as f:
+
+        with open(f_path) as f:
             f_str = f.read()
 
-        # Vertex Shader
-        self.vertex_shader = gl.glCreateShader(gl.GL_VERTEX_SHADER)
-        gl.glShaderSource(self.vertex_shader, v_str)
-        gl.glCompileShader(self.vertex_shader)
+        v_shader = shaders.compileShader(v_str, GL_VERTEX_SHADER)
+        f_shader = shaders.compileShader(f_str, GL_FRAGMENT_SHADER)
 
-        # Fragment Shader
-        self.fragment_shader = gl.glCreateShader(gl.GL_FRAGMENT_SHADER)
-        gl.glShaderSource(self.fragment_shader, f_str)
-        gl.glCompileShader(self.fragment_shader)
+        program = 0
+        try:
+            program = shaders.compileProgram(v_shader, f_shader)
+        except:
+            glDeleteShader(v_shader)
+            glDeleteShader(f_shader)
+        return program
+        
+    @staticmethod
+    def __init_buffers(vertices):
+        vao = glGenVertexArrays(1)
+        glBindVertexArray(vao)
 
-        # Shader Program
-        self.program = gl.glCreateProgram()
-        gl.glAttachShader(self.program, self.vertex_shader)
-        gl.glAttachShader(self.program, self.fragment_shader)
-        gl.glLinkProgram(self.program)
+        vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, vbo)
+        glBufferData(GL_ARRAY_BUFFER, 4 * len(vertices), vertices,
+                        GL_STATIC_DRAW)
 
-        # Clean up
-        gl.glDeleteShader(self.vertex_shader)
-        gl.glDeleteShader(self.fragment_shader)
+        glVertexAttribPointer(0, 3, GL_FLOAT, False, 20, None)
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(1, 2, GL_FLOAT, False, 20, c_void_p(12))
+        glEnableVertexAttribArray(1)
 
-        vertices = (
+        glBindVertexArray(0)
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+        return vao, vbo
+
+    @staticmethod
+    def __init_textures(t_path, program):
+        img = Image.open(t_path)
+        img_data = np.array(list(img.getdata()), np.int8)
+        texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        glUseProgram(program)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.width, img.height,
+                     0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+        glUniform1i(glGetUniformLocation(program, "u_texture"), 0)
+
+        return texture
+
+
+    def initializeGL(self):
+        glClearColor(0.5, 0, 0, 1.0)
+
+        vertices = np.array([
             1.0,  1.0, 0.0, 1.0, 1.0,
             1.0, -1.0, 0.0, 1.0, 0.0,
             -1.0, -1.0, 0.0, 0.0, 0.0,
             -1.0, -1.0, 0.0, 0.0, 0.0,
             -1.0,  1.0, 0.0, 0.0, 1.0,
             1.0,  1.0, 0.0, 1.0, 1.0
-        )
-        vertices = np.array(vertices, dtype=np.float32)
+        ], dtype=np.float32)
 
-        self.vao = gl.glGenVertexArrays(1)
-        self.vbo = gl.glGenBuffers(1)
+        self.vao, self.vbo = GLWidget.__init_buffers(vertices)
 
-        # Bind VAO
-        gl.glBindVertexArray(self.vao)
+        self.program = GLWidget.__init_shaders("shaders/basic.vert",
+                                               "shaders/basic.frag")
 
-        # Bind VBO
-        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vbo)
-        gl.glBufferData(gl.GL_ARRAY_BUFFER, 4 * len(vertices), vertices,
-                        gl.GL_STATIC_DRAW)
-
-        stride = 5 * 4
-        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE,
-                                 stride, None)
-        gl.glEnableVertexAttribArray(0)
-        gl.glVertexAttribPointer(1, 2, gl.GL_FLOAT, gl.GL_FALSE,
-                                 stride, ctypes.c_void_p(12))
-        gl.glEnableVertexAttribArray(1)
+        self.texture = GLWidget.__init_textures("samples/testcard.png",
+                                                self.program)
 
     def resizeGL(self, w, h):
-        gl.glViewport(0, 0, w, h)
+        glViewport(0, 0, w, h)
 
     def paintGL(self):
-        gl.glClearColor(0.5, 0, 0, 1.0)
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+        glClear(GL_COLOR_BUFFER_BIT)
 
-        gl.glUseProgram(self.program)
-        gl.glBindVertexArray(self.vao)
-        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 6)
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+
+        glUseProgram(self.program)
+        glBindVertexArray(self.vao)
+
+        glDrawArrays(GL_TRIANGLES, 0, 6)
